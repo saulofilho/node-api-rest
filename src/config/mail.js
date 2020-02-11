3ef9b96fd602e80adb7127c0b0b94987f65825ee
SauloFilho <saulosilvaf@gmail.com>
2020-02-11 19:15:36 -0300

    new
    appointment, notification, schedule and handlebars;

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b223b36df2333b',
+    pass: '9727eb8af3769d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
